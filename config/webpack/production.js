5197ef1471ddeb60b30aaf1e0253efe1ce142726
Matthew Carter <matty135792@gmail.com>
2021-09-30 15:27:49 +0100

    heroku webpacker config

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,4 +2,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
+webpack_compile_output = true
+
 module.exports = environment.toWebpackConfig()
