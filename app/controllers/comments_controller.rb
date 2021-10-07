be81631f9e8637b4efa69baa751a0fa46c29dff3
Matthew Carter <matty135792@gmail.com>
2021-10-07 20:13:52 +0100

    comment validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,39 @@
 class CommentsController < ApplicationController
   def create
-    @article = Article.find(params[:article_id])
-    # @comment = @article.comments.build(comment_params)
+    if Current.user
+      if can_comment?
+        @article = Article.find(params[:article_id])
+        # @comment = @article.comments.build(comment_params)
 
-    @comment = Comment.new(comment_params)
-    @comment.article = @article
+        @comment = Comment.new(comment_params)
+        @comment.article = @article
 
-    if @comment.save
-      redirect_to article_path(@article), success: 'comment created'
+        if @comment.save
+          redirect_to article_path(@article), success: 'comment created'
+        else
+          render :new
+        end
+      else
+        redirect_to article_path(@article), success: 'Not autherised to comment'
+      end
     else
-      render :new
+      redirect_to article_path(@article), success: 'Login to comment'
     end
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
-    @comment = @article.comments.find(params[:id])
+    if Current.user
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
 
-    if(Current.user.email == @comment.commenter)
-      @comment.destroy
-      redirect_to article_path(@article)
+      if(Current.user.email == @comment.commenter)
+        @comment.destroy
+        redirect_to article_path(@article)
+      else
+        redirect_to article_path(@article), alert: "Not Authorised to delete this"
+      end
     else
-      redirect_to article_path(@article), alert: "Not Authorised to delete this"
+      redirect_to article_path(@article), success: 'Login to delete'
     end
   end
 
@@ -32,5 +44,9 @@ class CommentsController < ApplicationController
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body, :status)
+    end
+
+    def can_comment?
+      Current.user.has_permission?('comment') || Current.user.has_permission?('admin')
     end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,23 @@
 module UsersHelper
+
+    def user_logged_in
+        Current.user ? true : false
+    end
+
+    def user_is_admin(user)
+        user.has_permission?('admin')
+    end
+
+    def current_user_is_admin
+        user_logged_in ? user_is_admin(Current.user) : false
+    end
+
+    def user_can_comment(user)
+        user.has_permission?('comment') || user.has_permission?('admin')
+    end
+
+    def current_user_can_comment
+        user_logged_in ? user_can_comment(Current.user) : false
+    end
+
 end
